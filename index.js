2c33d8ef9d9c9f789dce1c3bdb9d9918505aa803
pijushbarai <pijushbarai1@gmail.com>
2022-05-11 02:37:42 +0600

    css added in right side

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ document.getElementById("signupInLogin").addEventListener("click", ()=> {
     loginDiv.style.display = "none";
     signupDiv.style.display = "inline";
 })
+
+document.getElementById("createAccountBtn").addEventListener("click", ()=> {
+    alert("Hi i am in signup div");
+})
+
+document.getElementById("loginBtnInLogInPage").addEventListener("click", ()=> {
+    alert("Hi i am login btn in login div");
+})
